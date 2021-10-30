72f6638a3b43de79b6f6b088ff51dc7002d5eca0
Danilo Juvinao <proyectos@ibcaribe.com>
2021-10-29 19:30:35 -0500

    commit con el grupo g26

--- src/test/java/co/edu/usa/sbsemana3/CategoriaServiceTest.java
+++ src/test/java/co/edu/usa/sbsemana3/CategoriaServiceTest.java
@@ -9,6 +9,7 @@ import co.edu.usa.sbsemana3.modelo.Categoria;
 import co.edu.usa.sbsemana3.servicios.CategoriaService;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
@@ -37,7 +38,7 @@ public class CategoriaServiceTest {
 
     @Test
     @Order(1)
-    public void getAllTestEmpty() {
+    public void getAllEmpty() {
         System.out.println("getAllTestEmpty");
         List<Categoria> cats = service.getAll();
         Assertions.assertEquals(0, cats.size());
@@ -45,7 +46,7 @@ public class CategoriaServiceTest {
 
     @Test
     @Order(2)
-    public void getSaveTestThreeAndValidate() {
+    public void saveThreeAndValidate() {
         System.out.println("getSaveTestThreeAndValidate");
         Categoria cat1 = new Categoria();
         cat1.setNombre("Alimentos");
@@ -53,7 +54,7 @@ public class CategoriaServiceTest {
         cat2.setNombre("Alimentos");
         Categoria cat3 = new Categoria();
         cat3.setNombre("Alimentos");
-        
+
         service.save(cat1);
         service.save(cat2);
         service.save(cat3);
@@ -64,7 +65,7 @@ public class CategoriaServiceTest {
 
     @Test
     @Order(3)
-    public void getUpdateAndValidate() {
+    public void updateAndValidate() {
         System.out.println("getUpdateAndValidate");
         Categoria cat = service.getById(1);
         cat.setNombre("Vehiculos");
@@ -75,4 +76,49 @@ public class CategoriaServiceTest {
         Categoria catReconsultado = service.getById(1);
         Assertions.assertEquals("Vehiculos", catReconsultado.getNombre());
     }
+
+    @Test
+    @Order(4)
+    public void deleteAndValidate() {
+        boolean res = service.delete(1);
+        Assertions.assertEquals(true, res);
+        List<Categoria> cats = service.getAll();
+        Assertions.assertEquals(2, cats.size());
+    }
+
+    @Test
+    @Order(5)
+    public void tryDeleteNoValid() {
+        boolean res = service.delete(100);
+        Assertions.assertEquals(false, res);
+        List<Categoria> cats = service.getAll();
+        Assertions.assertEquals(2, cats.size());
+    } 
+
+    @Test
+    @Order(6)
+    public void tryGetByIdValid() {
+        Categoria categoria = service.getById(2);
+        Assertions.assertNotNull(categoria);
+    }
+
+    @Test
+    @Order(7)
+    public void tryGetByIdNoValid() {
+        Categoria categoria = service.getById(85);
+        Assertions.assertNull(categoria);
+    }
+
+    @Test
+    @Order(8)
+    public void tryUpdateWithNoValidId() {
+        Categoria categoria = new Categoria();
+        categoria.setCodigo(60);
+        categoria.setNombre("test category");
+        service.update(categoria);
+        Categoria cat = service.getById(60);
+        Assertions.assertNull(cat);
+
+    }
+
 }
